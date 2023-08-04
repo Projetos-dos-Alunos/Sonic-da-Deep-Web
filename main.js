162e9ec67ba1864090a8c556d9d7523fca7d3d2f
Projetos-dos-Alunos <141456283+Projetos-dos-Alunos@users.noreply.github.com>
2023-08-04 18:14:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function bodyload() {
+    document.getElementById("body").style;
+}
+
+function stepegou() {
+    document.getElementById("body").style.backgroundColor = " 5e0909";
+    document.getElementById("title").style.color = "470707";
+    document.getElementById("bla").style.color = "470707";
+    document.getElementById("dw").style.color = "470707";
+    document.getElementById("mm").style.color = "470707";
+    document.getElementById("b").style.backgroundColor = "5e0909";
+}
+function muda() {
+    document.getElementById("sonicdw").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAKR3iB8dzZX_xdE6DWx16IA1t6OwRQ4CuZQ&usqp=CAU"
+}
+function mudadenovo() {
+    document.getElementById("sonicdw").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwlvvgtNYpir0JYDGJnST94oUSv9XlPxgXzUr8Ng8&s"
+}
